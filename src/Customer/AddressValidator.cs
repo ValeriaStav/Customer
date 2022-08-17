697b230efe17e4a08bfa5e3a5be88c553fb44f9e
ValeriaStav <ValeriiaStavriadi@gmail.com>
2022-08-17 12:49:53 +0300

    FluentValidation

--- src/Customer/AddressValidator.cs
+++ src/Customer/AddressValidator.cs
@@ -1,52 +1,34 @@
-﻿namespace Customer
+﻿using FluentValidation;
+
+namespace Customer
 {
-    public class AddressValidator
+    public class AddressValidator : AbstractValidator<Address>
     {
         private const int AddressLineMaxLength = 100;
         private const int CityMaxLength = 50;
-        private static readonly List<string> AllowedCountries = new List<string>() { "United States", "Canada" };
+        private static readonly List<string> AllowedCountries = new() { "United States", "Canada" };
         private const int PostalCodeMaxLength = 6;
         private const int StateMaxLength = 20;
 
-        public static List<string> Validate(Address address)
+
+        public AddressValidator()
         {
-            var errors = new List<string>();
 
-            if (NewMethod(address))
-            {
-                errors.Add("Invalid address property: AddressLine");
-            }
-            if (address.AddressLine2?.Length > AddressLineMaxLength)
-            {
-                errors.Add("Invalid address property: AddressLine2");
-            }
-            if (address.AddressType == AddressType.Unknown)
-            {
-                errors.Add("Invalid address property: AddressType");
-            }
-            if (string.IsNullOrWhiteSpace(address.City) || address.City.Length > CityMaxLength)
-            {
-                errors.Add("Invalid address property: City");
-            }
-            if (!AllowedCountries.Contains(address.Country, StringComparer.OrdinalIgnoreCase))
-            {
-                errors.Add("Invalid address property: Country");
-            }
-            if (string.IsNullOrWhiteSpace(address.PostalCode) || address.PostalCode.Length > PostalCodeMaxLength)
-            {
-                errors.Add("Invalid address property: PostalCode");
-            }
-            if (string.IsNullOrWhiteSpace(address.State) || address.State.Length > StateMaxLength)
-            {
-                errors.Add("Invalid address property: State");
-            }
+            RuleFor(address => address.AddressLine).NotNull().MaximumLength(AddressLineMaxLength).WithMessage("Invalid address property: AddressLine");
 
-            return errors;
-        }
+            RuleFor(address => address.AddressLine2).MaximumLength(AddressLineMaxLength).WithMessage("Invalid address property: AddressLine2");
 
-        private static bool NewMethod(Address address)
-        {
-            return string.IsNullOrWhiteSpace(address.AddressLine) || address.AddressLine.Length > AddressLineMaxLength;
+            RuleFor(address => address.AddressType).NotEqual(AddressType.Unknown).WithMessage("Invalid address property: AddressType");
+
+            RuleFor(address => address.City).MaximumLength(CityMaxLength).WithMessage("Invalid address property: City");
+
+            RuleFor(address => address.Country).Must(country => AllowedCountries.Contains(country, StringComparer.OrdinalIgnoreCase)).WithMessage("Invalid address property: Country");
+
+            RuleFor(address => address.PostalCode).MaximumLength(PostalCodeMaxLength).WithMessage("Invalid address property: PostalCode");
+
+            RuleFor(address => address.State).MaximumLength(StateMaxLength).WithMessage("Invalid address property: State");
+
         }
     }
+
 }
